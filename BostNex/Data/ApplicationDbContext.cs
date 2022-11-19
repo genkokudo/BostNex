6ad8e5c539030bc66a49ad6aa8a4238c5c19e39d
ginpay <y-toyoda@live.jp>
2022-11-19 12:16:55 +0900

    BostNexShared追加

--- BostNex/Data/ApplicationDbContext.cs
+++ BostNex/Data/ApplicationDbContext.cs
@@ -1,10 +1,14 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using BostNexShared.Entity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BostNex.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        /// <summary>汎用的データ</summary>
+        public DbSet<General> Generals { get; set; }
+        
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
---
+++ BostNexShared/Entity/General.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BostNexShared.Entity
+{
+    /// <summary>
+    /// 汎用的なデータを格納
+    /// </summary>
+    public record General
+    {
+        [Key]
+        public long Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Value { get; set; }
+
+        public DateTime CreatedTime { get; set; }
+
+        public DateTime UpdatedTime { get; set; }
+    }
+}
