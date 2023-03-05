774ec33719a6321586055eed0d7cac0435f03c31
ginpay <y-toyoda@live.jp>
2023-03-05 14:30:51 +0900

    ツンデレ

--- BostNex/Services/OpenAiService.cs
+++ BostNex/Services/OpenAiService.cs
@@ -15,11 +15,12 @@ namespace BostNex.Services
     public interface IOpenAiService
     {
         /// <summary>
-        /// これまでのやり取りをリセットして最初からにします。
+        /// これまでのやり取りをリセットして最初からにする
+        /// プロンプトがあれば再設定する、無ければそのまま
         /// </summary>
-        /// <param name="prompt">最初の指示</param>
-        public void InitializeChat(string prompt);
-        
+        /// <param name="prompt">プロンプト</param>
+        public void InitializeChat(List<ChatPrompt> prompts = null!);
+
         /// <summary>
         /// ユーザの入力を受け取って、セッションを進める
         /// </summary>
@@ -36,10 +37,6 @@ namespace BostNex.Services
 
     public class OpenAiService : IOpenAiService
     {
-        private readonly string RoleUser = "user";
-        private readonly string RoleSystem = "system";
-        private readonly string RoleAssistant = "assistant";
-
         /// <summary>
         /// API
         /// クライアント側でも使えるようpublicにしておく
@@ -48,40 +45,59 @@ namespace BostNex.Services
 
         private readonly OpenAiOption _options;
 
-        private readonly List<ChatPrompt> _chatPrompts = new();
+        /// <summary>
+        /// チャットプロンプト
+        /// リセット時にこのプロンプトで設定する
+        /// </summary>
+        private List<ChatPrompt> _chatPrompts = new();
+
+        /// <summary>
+        /// 今までのチャットログ、プロンプトは含まない
+        /// カットしていない全ての生データをここに保持する
+        /// </summary>
+        private readonly List<ChatPrompt> _chatLogs = new();
 
         public OpenAiService(IOptions<OpenAiOption> options)
         {
             _options = options.Value;
             Api = new OpenAIClient(new OpenAIAuthentication(_options.ApiKey));
-
             InitializeChat();
         }
         
-        public void InitializeChat(string prompt = null!)
+        public void InitializeChat(List<ChatPrompt> prompts = null!)
         {
-            _chatPrompts.Clear();
-            if (!string.IsNullOrWhiteSpace(prompt))
+            // プロンプト再設定、無かったらそのまま
+            if (prompts != null)
             {
-                _chatPrompts.Add(new ChatPrompt(RoleSystem, prompt));
+                _chatPrompts = prompts;
             }
+            _chatLogs.Clear();
         }
 
         public async Task<string> GetNextSessionAsync(string input)
         {
-            _chatPrompts.Add(new ChatPrompt(RoleUser, input));
-            var chatRequest = new ChatRequest(_chatPrompts);
+            _chatLogs.Add(new ChatPrompt(ChatRoles.user.ToString(), input));
+            var chatRequest = new ChatRequest(GetAllChat(), maxTokens: _options.MaxTokens);
             var result = await Api.ChatEndpoint.GetCompletionAsync(chatRequest);
-            _chatPrompts.Add(new ChatPrompt(RoleAssistant, result.FirstChoice));
+            _chatLogs.Add(new ChatPrompt(ChatRoles.assistant.ToString(), result.FirstChoice));
 
             return result.FirstChoice;
         }
 
-
+        /// <summary>
+        /// 現状、GPTに送るチャットログを取得する
+        /// プロンプトと件数を制限した会話ログを連結する
+        /// </summary>
+        /// <returns></returns>
+        private List<ChatPrompt> GetAllChat()
+        {
+            var result = new List<ChatPrompt>();
+            result.AddRange(_chatPrompts);
+            result.AddRange(_chatLogs.Skip(Math.Max(0, _chatLogs.Count - _options.MaxChatLogCount)));   //_chatLogsの件数を新しい方から指定件数取る
+            return result;
+        }
 
     }
-
-
 
     /// <summary>
     /// 設定項目
@@ -93,6 +109,29 @@ namespace BostNex.Services
         /// OpenAIのアカウントでログインして取得すること
         /// </summary>
         public string ApiKey { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 保持するユーザとアシスタントのチャットログの上限を設定してトークン数を節約する。
+        /// </summary>
+        public int MaxChatLogCount { get; set; } = 10;
+
+        /// <summary>
+        /// 返答のトークン上限を設定してトークン数を節約する。
+        /// </summary>
+        public int MaxTokens { get; set; } = 180;
     }
 
+    /// <summary>
+    /// チャットの役割
+    /// ToStringをして使う
+    /// </summary>
+    public enum ChatRoles
+    {
+        /// <summary>最初の指示のみ使用する</summary>
+        system,
+        /// <summary>ユーザの入力</summary>
+        user,
+        /// <summary>GPTからの返答</summary>
+        assistant
+    }
 }
--- BostNex/Services/TrpgService.cs
+++ BostNex/Services/TrpgService.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Options;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using OpenAI.Chat;
 using OpenAI.Completions;
 using System.Text;
 
@@ -13,30 +14,42 @@ namespace BostNex.Services
     /// </summary>
     public interface ITrpgService
     {
-        /// <summary>
-        /// TRPG用のリクエストを取得する
-        /// </summary>
-        /// <param name="input"></param>
-        /// <returns></returns>
-        public CompletionRequest GetTrpgRequest(string input);
-
         // TODO:プロンプトを作成していくこと。
+        public List<ChatPrompt> Tsunko { get; }
     }
 
     public class TrpgService : ITrpgService
     {
-        private readonly string Tsunko = "ツン子という少女を相手にした対話のシミュレーションを行います。\r\n彼女の発言サンプルを以下に列挙します。\r\n\r\nあんたのことなんか、どうでもいいわ！\r\nうっさい！黙ってて！\r\nこんなの、私がやるわけないじゃない！\r\nお、おい…馬鹿にしないでよね。\r\nう、うっかり…気にしないでよね！\r\nあんたとは話しているつもりじゃないわよ。\r\nな、なんでそんなに見つめないでよ！\r\nうぅ…ちょっと待って、私、もう一回言ってあげるからね。\r\nあんた、そこに立ってないで、何かしてよ！\r\nほ、本当に私がこんなことするわけないでしょう？\r\nうっさい！邪魔しないで！\r\nあんたの言うことなんて、どうだっていいわ！\r\nち、違うってば！私、全然…！\r\nべ、別にあんたが好きだからって言ってるわけじゃないんだからね！\r\nな、何よ、いきなり抱きついてきて…っ！\r\nあんたみたいな人と一緒にいると、本当に疲れるわ。\r\nそ、そんなに急かさないでよ…！\r\nあんた、いつもいい加減なこと言うわね。\r\nうっさい！うるさいってば！\r\nあんたのことなんて、どうでもいいからさっさと帰って！\r\n\r\n上記例を参考に、ツン子の性格や口調、言葉の作り方を模倣し、回答を構築してください。\r\nではシミュレーションを開始します。";
-        public CompletionRequest GetTrpgRequest(string input)
+        List<ChatPrompt> ITrpgService.Tsunko => tsunko;
+        private List<ChatPrompt> tsunko = new()
         {
-            return new CompletionRequest(
-                input,
-                //OpenAI_API.Models.Model.DavinciText,
-                //200,    // 289文字ぐらい出してくれた。ここの文字数でお金がかかるので、早めにプロンプトで字数制限を付けるべき。
-                //0.5,
-                presencePenalty: 0.1,
-                frequencyPenalty: 0.1
-            );
-        }
+            new ChatPrompt(ChatRoles.system.ToString(),
+                "ツン子という少女を相手にした対話のシミュレーションを行います。\r\n" +
+                "彼女の発言サンプルを以下に列挙します。\r\n" +
+                "\r\n" +
+                "あんたのことなんか、どうでもいいわ！\r\n" +
+                "うっさい！黙ってて！\r\n" +
+                "こんなの、私がやるわけないじゃない！\r\n" +
+                "お、おい…馬鹿にしないでよね。\r\n" +
+                "う、うっかり…気にしないでよね！\r\n" +
+                "あんたとは話しているつもりじゃないわよ。\r\n" +
+                "な、なんでそんなに見つめないでよ！\r\n" +
+                "うぅ…ちょっと待って、私、もう一回言ってあげるからね。\r\n" +
+                "あんた、そこに立ってないで、何かしてよ！\r\n" +
+                "ほ、本当に私がこんなことするわけないでしょう？\r\n" +
+                "うっさい！邪魔しないで！\r\nあんたの言うことなんて、どうだっていいわ！\r\n" +
+                "ち、違うってば！私、全然…！\r\n" +
+                "べ、別にあんたが好きだからって言ってるわけじゃないんだからね！\r\n" +
+                "な、何よ、いきなり抱きついてきて…っ！\r\n" +
+                "あんたみたいな人と一緒にいると、本当に疲れるわ。\r\n" +
+                "そ、そんなに急かさないでよ…！\r\n" +
+                "あんた、いつもいい加減なこと言うわね。\r\n" +
+                "うっさい！うるさいってば！\r\n" +
+                "あんたのことなんて、どうでもいいからさっさと帰って！\r\n" +
+                "\r\n" +
+                "上記例を参考に、ツン子の性格や口調、言葉の作り方を模倣し、回答を構築してください。\r\n" +
+                "ではシミュレーションを開始します。")
+        };
     }
 
 }
